e6ceb4c78d882e1ba4c5843675af7f6041e19d2f
Lakshya Kumrawat <73424857+Lakshya8600@users.noreply.github.com>
2023-01-03 20:24:24 +0530

    Added some strategies

---
+++ NearVWAP.py
@@ -0,0 +1,59 @@
+from datetime import date
+import pandas as pd
+import numpy as np
+import yfinance as yf
+import os
+# from plyer import notification
+import pandas_ta as ta
+
+CompanyNames = pd.read_csv('StockData/CompanyNamestop400.csv')
+TotalCompany = 0
+CompanyDataList = os.listdir(os.getcwd()+"/StockData") 
+
+def speak(audio):
+       # This command is to change voice 
+       # espeak -ven-us+f4 -s170 "6 new wireless networks found"
+       
+       # engine = pyttsx3.init('sapi5')
+       # voices = engine.getProperty('voices') #getting details of current voice
+       # engine.setProperty('voice', voice[0].id)
+
+       os.system('espeak'+' "'+audio+'"')  
+
+# speak("hello")
+
+for Company in CompanyNames.iloc[:,0]:
+    try:
+        data = yf.download(tickers=Company,start=date.today(), interval='1m')
+        # Timeopen = data.iloc[-8,0]
+        # Timeclose = data.iloc[-1,3]
+    except:
+        pass
+    
+
+    # percentchange = 0
+    # if Timeopen >= Timeclose:
+    #     percentchange = ((Timeopen-Timeclose)/Timeclose)*100
+    # else:
+    #     percentchange = ((Timeclose-Timeopen)/Timeopen)*100
+
+    # print(data)
+    # print(data.iloc[-8,0])
+    # print(data.iloc[-1,3]
+
+    # print(ta.Volume)
+    df = pd.DataFrame().ta.ticker("AAPL") 
+    
+    # f = open('result.txt', 'at')
+    # if percentchange > 0.7:
+    #     f.write(Company)
+    #     f.write("\n")
+    #     f.write(str(percentchange))
+    #     f.write("\n")
+    #     f.write("\n")
+    #     # notification.notify(title = Company,message=str(percentchange) ,timeout=2)
+    #     speak(Company)
+    #     print(Company)
+    #     print(percentchange)
+
+    # f.close()
